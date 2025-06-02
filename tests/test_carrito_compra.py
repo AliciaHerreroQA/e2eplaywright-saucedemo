a28db8f157aeb7dd6fba1a8c0d52a6b61987a083
Alicia <alihersan@alu.edu.gva.es>
2025-06-02 19:25:35 +0200

    últimos cambios

--- tests/test_carrito_compra.py
+++ tests/test_carrito_compra.py
@@ -4,56 +4,56 @@ from playwright.sync_api import Page, expect
 def test_comprar_datoscorrectos(page: Page):
     page.goto("https://www.saucedemo.com/")
 
-    # Login
+    print("Given el usuario prueba a comprar un articulo y los datos son correctos")
     page.locator('[data-test="username"]').fill("standard_user")
     page.locator('[data-test="password"]').fill("secret_sauce")
     page.locator('[data-test="login-button"]').click()
 
-    #Añado artículo
+    print("When el usuario añade un artículo al carrito")
     page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click()
 
-    #Voy al carrito
+    print("And el usuario va al carrito de la compra")
     page.locator('.shopping_cart_link').click()
 
-    #Inicio checkout
+    print("And el usuario inicia la compra de los articulos")
     page.locator('[data-test="checkout"]').click()
 
-    #Relleno datos correctos
+    print("And el usuario rellena sus datos correctamente")
     page.locator('[data-test="firstName"]').fill("Alicia")
     page.locator('[data-test="lastName"]').fill("Tester")
     page.locator('[data-test="postalCode"]').fill("23456")
     page.locator('[data-test="continue"]').click()
 
-    #Finalizo compra
+    print("And el usuario finaliza la compra")
     page.locator('[data-test="finish"]').click()
 
-    #Verifico mensaje de éxito
+    print("And el usuario comprueba que su compra se ha realizado con éxito")
     expect(page.locator("h2")).to_have_text("Thank you for your order!")
 
 
 def test_comprar_datosincorrectos(page: Page):
     page.goto("https://www.saucedemo.com/")
 
-    #Login
+    print("Given el usuario prueba a comprar un articulo y los datos son incorrectos")
     page.locator('[data-test="username"]').fill("standard_user")
     page.locator('[data-test="password"]').fill("secret_sauce")
     page.locator('[data-test="login-button"]').click()
 
-    #Añado artículo
+    print("When el usuario añade un artículo al carrito")
     page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click()
 
-    #Voy al carrito
+    print("And el usuario va al carrito de la compra")
     page.locator('.shopping_cart_link').click()
     page.locator('[data-test="checkout"]').click()
 
-    #Dejo campos vacíos
+    print("And el usuario deja algun campo vacio")
     page.locator('[data-test="firstName"]').fill("")
     page.locator('[data-test="lastName"]').fill("Tester")
     page.locator('[data-test="postalCode"]').fill("")
 
     page.locator('[data-test="continue"]').click()
 
-    #Verifico mensaje de error
+    print("And el usuario comprueba que su compra no se ha podido realizar")
     expect(page.locator('[data-test="error"]')).to_be_visible()
 
  
--- tests/test_formulariologin.py
+++ tests/test_formulariologin.py
@@ -6,15 +6,18 @@ def test_usuario_valido(page:Page):
     page.get_by_placeholder("Username").type("standard_user")
     page.get_by_placeholder("Password").type("secret_sauce")
     page.get_by_role("button", name="Login").click()
+    
+    expect(page.get_by_text("Products")).to_be_visible()
 
 
 
 def test_contraseña_incorrecta(page:Page):
     print("And el usuario prueba que la contraseña sea valida")
     page.goto("https://www.saucedemo.com/")
+    page.get_by_placeholder("Username").type("standard_user")
     page.get_by_placeholder("Password").type("malacontraseña")
     page.get_by_role("button", name="Login").click()
-    expect(page.get_by_text("Epic sadface: Username and password do not match any user in this service")).to_be_visible
+    expect(page.get_by_text("Epic sadface: Username and password do not match any user in this service")).to_be_visible()
 
 
 def test_usuario_contraseña_vacio(page:Page):
@@ -22,4 +25,4 @@ def test_usuario_contraseña_vacio(page:Page):
     page.goto("https://www.saucedemo.com/")
     page.get_by_role("button", name="Login").click()
     
-    expect(page.get_by_text("Username is required")).to_be_visible
+    expect(page.get_by_text("Username is required")).to_be_visible()
